670a945fe2ce757ad79eac5cbab337f82c97bdfc
Christian Castillo <cl.castillo@samsung.com>
2019-07-18 13:27:44 +0800

    update semilotion.java

--- src/com/theclcode/bitcombinations/Semilotion.java
+++ src/com/theclcode/bitcombinations/Semilotion.java
@@ -14,11 +14,13 @@ public class Semilotion {
     }
 
     public static void main(String[] args){
+        findCombinations(5, 2);
+    }
 
-       int k = 5;
-       Vector<String>[][] DP = new Vector[k+1][k+1];
-       init(DP, k);
-       String prefix = "";
+    public static void findCombinations(int k, int ones){
+        Vector<String>[][] DP = new Vector[k+1][k+1];
+        init(DP, k);
+        String prefix = "";
 
         for (int len=0; len <= k; len++)
         {
@@ -26,31 +28,20 @@ public class Semilotion {
             prefix = prefix + "0";
         }
 
-        for (int len = 1; len <= k; len++)
-        {
-            for (int n = 1; n <= len; n++)
-            {
-                // prefix 0 to all combinations of length len-1
-                // with n ones
-                for (String str : DP[len - 1][n])
-                    DP[len][n].add("0" + str);
+        for (int len = 1; len <= k; len++) {
+            for (int n = 1; n <= len; n++) {
+                for (String str : DP[len - 1][n]){
 
-                // prefix 1 to all combinations of length len-1
-                // with n-1 ones
-                for (String str : DP[len - 1][n - 1])
+                    DP[len][n].add("0" + str);
+                }
+                for (String str : DP[len - 1][n - 1]) {
                     DP[len][n].add("1" + str);
+                }
             }
         }
 
-        System.out.println(DP[k]);
-
-//        for (int n = 1; n <= k; n++)
-//        {
-//            for (String str : DP[k][n]){
-//                System.out.print(str+" ");
-//            }
-//            System.out.println("");
-//        }
-
+        for (String str : DP[k][ones]){
+            System.out.println(str+" ");
+        }
     }
 }
