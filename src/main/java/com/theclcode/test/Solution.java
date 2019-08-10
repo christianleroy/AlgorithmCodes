d1d8f152081ea6e57531bfbd21b797f1b3ed08c0
Christian <christianleroydev@gmail.com>
2019-08-10 14:22:07 +0800

    Update Solution.java

--- src/main/java/com/theclcode/test/Solution.java
+++ src/main/java/com/theclcode/test/Solution.java
@@ -2,7 +2,8 @@ package com.theclcode.test;
 public class Solution {
 
     public static void main(String[] args){
-        String permutation="671-11-35";
+//        String permutation="671-11-35";
+        String permutation="69";
         boolean isOperator=false;
         String operation = "";
         String[] operators = new String[2];
@@ -41,6 +42,37 @@ public class Solution {
         for(int counter=0; counter<str.length; counter++){
             numbers[counter] = Integer.parseInt(str[counter]);
         }
+
+        if(index-1==-1){
+            System.out.print(numbers[0]);
+        } else if(index-1==0) {
+            System.out.print(evaluate(operators[0], numbers[0], numbers[1]));
+        } else if(index-1==1){
+            if((operators[0]=="+" || operators[0]=="-") && (operators[1]=="*" || operators[1]=="/")){
+                double result = evaluate(operators[1], numbers[1], numbers[2]);
+                result = evaluate(operators[0], numbers[0], result);
+                System.out.print(result);
+            } else {
+               double result = evaluate(operators[0], numbers[0], numbers[1]);
+               result = evaluate(operators[1], result, numbers[2]);
+               System.out.print(result);
+            }
+        }
+    }
+
+    public static double evaluate(String operator, double number1, double number2){
+        double result=0;
+        if(operator.equals("+")){
+            result=number1+number2;
+        } else if(operator.equals("-")){
+            result=number1-number2;
+        } else if(operator.equals("*")){
+            result=number1*number2;
+
+        } else if(operator.equals("/")){
+            result=number1/number2;
+        }
+        return result;
     }
 
 }
