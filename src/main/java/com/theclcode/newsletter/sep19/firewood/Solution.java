67116bb42c0b032d6e1784f7376360e120bb3089
Christian Castillo <cl.castillo@samsung.com>
2019-09-06 14:32:21 +0800

    beginning solution for firewood

---
+++ src/main/java/com/theclcode/newsletter/sep19/firewood/Solution.java
@@ -0,0 +1,113 @@
+package com.theclcode.newsletter.sep19.firewood;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static int trees=0;
+    public static Thing[] firesArray = new Thing[81];
+    public static int firesArrayStack = 0;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int testCasers = sc.nextInt();
+        for(int i=0; i<testCasers; i++){
+            init();
+            int gridSize = sc.nextInt();
+            int treesToCut = sc.nextInt();
+            int thingNum = sc.nextInt();
+            int[][] forest = buildForest(thingNum, gridSize);
+            if(firesArrayStack==0){
+                System.out.println(trees);
+            } else {
+                getMaxTrees(forest, treesToCut);
+            }
+        }
+    }
+
+    public static int getMaxTrees(int[][] forest, int treesToCut){
+        int[][] forestCopy = copyArray(forest);
+        for(int i=0; i<firesArray.length; i++){
+
+        }
+        return 0;
+    }
+
+    public static void findPath(int[][] forest, int x, int y){
+
+    }
+
+
+    private static int[][] buildForest(int thingNum, int gridSize) {
+        int[][] forest = new int[gridSize][gridSize];
+        for(int j=0; j<gridSize; j++){
+            for(int k=0; k<gridSize; k++){
+                if(thingNum==1){
+                    trees++;
+                } else if(thingNum==2){
+                    Thing thing = new Thing(j, k, Type.FIRE);
+                    firesArray[firesArrayStack++] = thing;
+                }
+                forest[j][k]=thingNum;
+                System.out.print(forest[j][k]+" ");
+            }
+            System.out.println();
+        }
+        return forest;
+    }
+
+    private static int[][] copyArray(int[][] forest) {
+        int[][] forestCopy = new int[forest.length][forest.length];
+        for(int j=0; j<forest.length; j++) {
+            for (int k = 0; k < forest.length; k++) {
+                forestCopy[j][k]=forest[j][k];
+            }
+        }
+        return forestCopy;
+    }
+    private static void init() {
+        trees=0;
+        firesArray = new Thing[81];
+        firesArrayStack = 0;
+    }
+    public enum Type{
+        TREE,
+        FIRE,
+        EMPTY
+    }
+    public static class Thing {
+        int x;
+        int y;
+        Type type;
+
+        public Thing(int x, int y, Type type){
+            this.x=x;
+            this.y=y;
+            this.type=type;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public void setX(int x) {
+            this.x = x;
+        }
+
+        public int getY() {
+            return y;
+        }
+
+        public void setY(int y) {
+            this.y = y;
+        }
+
+        public Type getType() {
+            return type;
+        }
+
+        public void setType(Type type) {
+            this.type = type;
+        }
+    }
+}
