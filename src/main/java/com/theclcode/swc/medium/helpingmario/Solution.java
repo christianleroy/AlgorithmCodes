054380135d06959bb560ed2bc3da80d9a624fa89
Christian <christianleroydev@gmail.com>
2019-09-14 12:19:23 +0800

    hopeful

--- src/main/java/com/theclcode/swc/medium/helpingmario/Solution.java
+++ src/main/java/com/theclcode/swc/medium/helpingmario/Solution.java
@@ -4,53 +4,59 @@ import java.util.Scanner;
 
 public class Solution {
 
-    static boolean found = false;
+    static boolean hasFoundPath;
+    static boolean[][] grid;
+    static boolean[][] visited;
+    static boolean topRowOpen;
+    static int max;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int testCases = sc.nextInt();
         for(int i=1; i<=testCases; i++){
-            found = false;
-            int gridSize = sc.nextInt();
-            boolean[][] grid = new boolean[gridSize][gridSize];
-            int ticks=0;
-            for(int j=0; j<gridSize*gridSize; j++) {
-                int x = sc.nextInt() - 1;
-                int y = sc.nextInt() - 1;
-                grid[x][y] = true;
-
-                if(!found){ ticks=j; }
-
-                if(ticks>=gridSize){
-                    for(int k=0; k<gridSize && !found; k++){
-                        if(grid[0][k]){
-                            search(grid, new boolean[gridSize][gridSize], 0, k);
-                        }
+            max = sc.nextInt();
+            grid=new boolean[max][max];
+            topRowOpen=false;
+            hasFoundPath=false;
+            int x, y, tick=0;
+            for(int j=0; j<max*max; j++){
+                tick = !hasFoundPath ? j : tick;
+                grid[x=sc.nextInt()-1][y=sc.nextInt()-1]=true;
+                topRowOpen = x==0 ? true : topRowOpen;
+                if(tick>=max && topRowOpen && !hasFoundPath){
+                    visited=new boolean[max][max];
+                    for(int k=0; k<max; k++){
+                        search(0, k);
                     }
                 }
+
             }
-            System.out.println("#"+i+" "+(ticks+1));
+            System.out.println("#"+i+" "+(tick+1));
         }
     }
 
-    private static boolean search(boolean[][] grid, boolean[][] visited, int x, int y) {
+    public static void search(int x, int y){
         if(x==grid.length-1){
-            found = true;
-            return true;
+            hasFoundPath=true;
+            return;
+        } else {
+            if(x<grid.length-1 && grid[x+1][y] && !visited[x+1][y]){
+                visited[x+1][y]=true;
+                search(x+1, y);
+            }
+            if(y<grid.length-1 && grid[x][y+1] && !visited[x][y+1]){
+                visited[x][y+1]=true;
+                search(x,y+1);
+            }
+            if(y>0 && grid[x][y-1] && !visited[x][y-1]){
+                visited[x][y-1]=true;
+                search(x,y-1);
+            }
+            if(x>0 && grid[x-1][y] && !visited[x-1][y]){
+                visited[x-1][y]=true;
+                search(x-1,y);
+            }
         }
-        visited[x][y]=true;
-        if(x<grid.length-1 && grid[x+1][y] && !visited[x+1][y]){
-            search(grid, visited, x+1, y);
-        }
-        if(y<grid.length-1 && grid[x][y+1] && !visited[x][y+1]){
-            search(grid, visited, x, y+1);
-        }
-        if(y>0 && grid[x][y-1] && !visited[x][y-1]){
-            search(grid, visited, x, y-1);
-        }
-        if(x>0 && grid[x-1][y] && !visited[x-1][y]){
-            search(grid, visited, x-1, y);
-        }
-        return false;
+
     }
 }
--- src/main/java/com/theclcode/test/Solution.java
+++ src/main/java/com/theclcode/test/Solution.java
@@ -3,105 +3,18 @@ package com.theclcode.test;
 import java.util.*;
 
 public class Solution {
-    static class Node {
-        int id;
-        List<Node> adjacent = new ArrayList<>();
+    static int stackMarker;
+    static char[] stack;
 
-        public Node(int id) {
-            this.id = id;
-        }
-
-        public int getId() {
-            return id;
-        }
-
-        public void setId(int id) {
-            this.id = id;
-        }
-
-        public List<Node> getAdjacent() {
-            return adjacent;
-        }
-
-        public void setAdjacent(List<Node> adjacent) {
-            this.adjacent = adjacent;
-        }
-    }
-
-    public static void main(String[] args){
-        Node node1 = new Node(1);
-        Node node2 = new Node(2);
-        Node node3 = new Node(3);
-        Node node4 = new Node(4);
-        Node node5 = new Node(5);
-        Node node6 = new Node(6);
-        Node node7 = new Node(7);
-        Node node8 = new Node(8);
-        Node node9 = new Node(9);
-        Node node10 = new Node(10);
-        Node node11 = new Node(11);
-
-        node1.getAdjacent().add(node2);
-        node1.getAdjacent().add(node3);
-        node1.getAdjacent().add(node4);
-
-        node2.getAdjacent().add(node5);
-        node2.getAdjacent().add(node6);
-
-        node3.getAdjacent().add(node7);
-        node3.getAdjacent().add(node6);
-
-        node4.getAdjacent().add(node11);
-
-        node6.getAdjacent().add(node3);
-
-        node7.getAdjacent().add(node8);
-        node7.getAdjacent().add(node9);
-        node7.getAdjacent().add(node10);
-
-        Node node13 = new Node(13);
-
-        Node toFind = node13;
-//
-        if(!searchByDfs(node1, toFind, new ArrayList<>())){
-            System.out.println("Cannot find node " + toFind.getId());
-        }
-//
-//        searchByBfs(node1, toFind, new ArrayList<>(), new LinkedList<>());
-
-        char[] str = "abc".toCharArray();
+    public static void main(String[] args) {
+        char[] str = {'a','b','c'};
 //        permute(str, 0, str.length-1);
-//        combine(str, 0, 2);
 
-    }
-
-
-
-    public static boolean searchByDfs(Node source, Node destination, List<Integer> visited){
-        if(visited.contains(source.getId())){
-            return false;
-        }
-        System.out.println("Checking node "+source.getId()+"...");
-        if(source.getId()==destination.getId()){
-            System.out.println("Found destination "+destination.getId());
-            return true;
-        }
-
-        visited.add(source.getId());
-        System.out.println("Checking children of node "+source.getId());
-        for(Node adjacent : source.getAdjacent()){
-            if(searchByDfs(adjacent, destination, visited)){
-                return true;
-            }
-        }
-        return false;
-    }
+        stackMarker=0;
+        stack=new char[2];
+        combine(str, 0, 2);
 
 
-    public static void swap(char[] str, int a, int b){
-      char temp = str[a];
-      str[a] = str[b];
-      str[b] = temp;
     }
 
     public static void permute(char[] str, int start, int end){
@@ -110,27 +23,29 @@ public class Solution {
             return;
         }
 
-        for(int i=start; i<=end; i++){
+        for(int i=start; i<str.length; i++){
             swap(str, start, i);
             permute(str, start+1, end);
             swap(str, start, i);
         }
     }
 
-    static int limit = 3;
-    static char[] stack = new char[limit];
-    static int stackMarker = 0;
+    private static void swap(char[] str, int left, int right) {
+        char temp = str[right];
+        str[right] = str[left];
+        str[left] = temp;
+    }
 
     public static void combine(char[] str, int start, int limit){
         if(limit==0){
             System.out.println(new String(stack));
             return;
         }
-        for(int i=start; i<=str.length-limit; i++){
-           stack[stackMarker++] = str[i];
-           combine(str, i+1, limit-1);
-           stack[--stackMarker] = '\0';
-        }
 
+        for(int i=start; i<=str.length-limit; i++){
+            stack[stackMarker++]=str[i];
+            combine(str, i+1, limit-1);
+            stack[--stackMarker]='\0';
+        }
     }
 }
