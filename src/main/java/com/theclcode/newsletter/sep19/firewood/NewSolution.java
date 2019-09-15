24aa27ef6398fdc9f59e85a70230c1640ee1bf15
Christian <christianleroydev@gmail.com>
2019-09-15 08:33:10 +0800

    Finish NewSolution for Firewood problem.

--- src/main/java/com/theclcode/newsletter/sep19/firewood/NewSolution.java
+++ src/main/java/com/theclcode/newsletter/sep19/firewood/NewSolution.java
@@ -6,20 +6,25 @@ public class NewSolution {
 
     static ForestObject[] trees;
     static ForestObject[] fires;
-    static ForestObject[] treesStack;
-    static int treesStackMarker;
     static int[][] forest;
+    static ForestObject[] treesCombinationStack;
+    static int treesCombinationStackMarker;
     static int treeCutLimit=0;
+    static int maxTrees;
+
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int testCases = sc.nextInt();
         for(int tc=1; tc<=testCases; tc++){
             int size = sc.nextInt();
-            forest = new int[size][size];
-            treeCutLimit = sc.nextInt();
-            fires = new ForestObject[size*size];
             trees = new ForestObject[size*size];
+            fires = new ForestObject[size*size];
+            forest = new int[size][size];
+            treesCombinationStack = new ForestObject[2];
+            treesCombinationStackMarker = 0;
+            treeCutLimit = sc.nextInt();
+            maxTrees=0;
             int treesMarker=0, firesMarker=0;
             for(int i=0; i<size; i++){
                 for(int j=0; j<size; j++){
@@ -32,31 +37,36 @@ public class NewSolution {
                 }
             }
             if(firesMarker==0){
-                System.out.print("Case #"+tc+": "+treesMarker);
+                System.out.println("Case #"+tc+": "+treesMarker);
             } else {
-                int maxTrees = simulateFirePath(copyArray(), firesMarker, treesMarker);
+                int defaultMaxTrees = simulateFirePath(copyArray(), firesMarker, treesMarker);
+                getMaxTrees(firesMarker, treesMarker, treeCutLimit);
+                System.out.println("Case #"+tc+": "+(defaultMaxTrees > maxTrees ? defaultMaxTrees : maxTrees));
             }
         }
     }
 
-    private static int getMaxTrees(int fires, int remainingTrees) {
-        treesStack = new ForestObject[remainingTrees];
-        treesStackMarker=0;
+    private static int getMaxTrees(int fires, int remainingTrees, int treeCutLimit) {
+        for(int i=1; i<=treeCutLimit; i++){
+            treesCombinationStack = new ForestObject[i];
+            treesCombinationStackMarker=0;
+            combination(trees, 0, i, fires, remainingTrees);
+        }
         return 0;
     }
 
     public static int simulateFirePath(int[][] forestCopy, int firesCount, int treesCount){
         for(int i=0; i<firesCount; i++){
-            ForestObject[] stack = new ForestObject[4];
+            ForestObject[] stack = new ForestObject[forestCopy.length*forestCopy.length];
             int marker=0;
             stack[marker++] = fires[i];
             while(marker>0){
                 ForestObject fire = stack[--marker];
+                stack[marker]=null;
                 int x=fire.getX();
                 int y=fire.getY();
                 if(forestCopy[x][y]==1){
                     treesCount--;
-                    print(forestCopy);
                 }
                 forestCopy[x][y]=0;
                 if(y<forestCopy.length-1 && forestCopy[x][y+1]==1){
@@ -72,20 +82,32 @@ public class NewSolution {
                     stack[marker++]= new ForestObject(x-1, y);
                 }
             }
-            print(forestCopy);
         }
         return treesCount;
     }
 
-    public static void combination(ForestObject[] trees, int start, int limit){
+    public static void combination(ForestObject[] trees, int start, int limit, int firesCount, int treesCount){
         if(limit==0){
+            int[][] forestCopy = copyArray();
+            for(int i=0; i<treesCombinationStackMarker; i++){
+                ForestObject tree = treesCombinationStack[i];
+                forestCopy[tree.getX()][tree.getY()]=0;
+                treesCount--;
+            }
+            int remainingTrees = simulateFirePath(forestCopy, firesCount, treesCount);
+            if(remainingTrees>maxTrees){
+                maxTrees=remainingTrees;
+            }
             return;
         }
 
-        for(int i=start;i<trees.length-limit; i++){
-
+        for(int i=start;i<treesCount-limit; i++){
+            treesCombinationStack[treesCombinationStackMarker++] = trees[i];
+            combination(trees, i+1, limit-1, firesCount, treesCount);
+            treesCombinationStack[--treesCombinationStackMarker]=null;
         }
     }
+
     public static int[][] copyArray(){
         int[][] copy = new int[forest.length][forest.length];
         for(int x=0; x<forest.length; x++){
@@ -95,6 +117,7 @@ public class NewSolution {
         }
         return copy;
     }
+
     public static class ForestObject {
         private int x;
         private int y;
@@ -110,14 +133,5 @@ public class NewSolution {
         public int getY() {
             return y;
         }
-    }
-    private static void print(int[][] a){
-        for(int i=0; i<a.length; i++){
-            for(int j=0; j<a.length; j++){
-                System.out.print(a[i][j]+" ");
-            }
-            System.out.println();
-        }
-        System.out.println();
     }
 }
