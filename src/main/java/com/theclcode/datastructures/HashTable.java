4a6e82ac4d3f7bb42fa34ff97134580ec1a514f5
Christian Castillo <cl.castillo@samsung.com>
2019-10-17 09:49:14 +0800

    Complete working HashTable implementation.

--- src/main/java/com/theclcode/datastructures/HashTable.java
+++ src/main/java/com/theclcode/datastructures/HashTable.java
@@ -1,44 +1,83 @@
 package com.theclcode.datastructures;
 
 public class HashTable<K, V> {
-
-    LinkedList<K, V>[] table;
     int capacity;
-    static long[] powers = new long[12]; //assuming longest string is 12
+    Node<K, V>[] table;
+    static final int[] powers = new int[3];
     static final int BASE = 37;
     static {
         powers[0]=1;
         powers[1] = BASE;
-        for(int i=2; i<=11; i++){
-            powers[i] = powers[i-1]*BASE;
-        }
+        powers[2] = powers[1] * BASE;
     }
 
+    public HashTable(){
+        this(701);
+    }
+
+    @SuppressWarnings("unchecked")
     public HashTable(int capacity){
         this.capacity = capacity;
-        table = new LinkedList[capacity];
+        table = (Node<K, V>[]) new Node[capacity];
     }
 
     public void put(K key, V value){
-        int index = (int)(hash(key.toString()) % capacity);
-        index = index < 0 ? index*(-1) : index;
-        if(table[index]==null){
-            LinkedList linkedList = new LinkedList();
-            linkedList.add(new Node(key, value));
-            table[index] = linkedList;
+        int index = getAddress(key);
+        if(table[index] == null){
+            Node<K, V> node = new Node<>(key, value);
+            table[index] = node;
         } else {
-
+            Node<K, V> node = find(key);
+            if(node == null){
+                node = new Node<>(key, value);
+                Node<K, V> existingNode = table[index];
+                node.next = existingNode;
+                existingNode.prev = node;
+                table[index] = node;
+            } else {
+                node.value = value;
+            }
         }
     }
 
     public V get(K key){
-        int index = (int)(hash(key.toString()) % capacity);
-        if(table[index]!=null){
-            LinkedList<K, V> linkedList = table[index];
-            Node<K, V> node = linkedList.head;
-            while(node!=null){
-                if(key==node.key){
-                    return node.value;
+        Node<K, V> node = find(key);
+        if(node == null){
+            return null;
+        } else {
+            return node.value;
+        }
+    }
+
+    private Node<K, V> find(K key) {
+        int index = getAddress(key);
+        if(table[index] == null){
+            return null;
+        } else {
+            Node<K, V> node = table[index];
+            while (node != null) {
+                if(key == null && node.key == null){
+                    return node;
+                }
+                if (key instanceof String) {
+                    String _key = (String) key;
+                    String nodeKey = (String) node.key;
+                    if (hash(key) == hash(node.key) && _key.length() == nodeKey.length()) {
+                        boolean isEqual = true;
+                        for (char i = 0; i < _key.length(); i++) {
+                            if (_key.charAt(i) != nodeKey.charAt(i)) {
+                                isEqual = false;
+                                break;
+                            }
+                        }
+                        if(isEqual){
+                            return node;
+                        }
+                    }
+                } else {
+                    if(node.key.equals(key)){
+                        return node;
+                    }
                 }
                 node = node.next;
             }
@@ -46,59 +85,91 @@ public class HashTable<K, V> {
         return null;
     }
 
-    public long hash(String word){
+    public void remove(K key){
+        Node<K, V> node = find(key);
+        if(node != null){
+            if(node.next != null){
+                node.next.prev = node.prev;
+            }
+            if(node.prev != null){
+                node.prev.next = node.next;
+            }
+            int index = getAddress(key);
+            if(table[index] == node){
+                table[index] = null;
+            }
+        }
+    }
+
+    private int getAddress(K key){
+        int index = key == null ? 0 : hash(key) % capacity;
+        return index;
+    }
+
+    int hash(K key){
+        String word = key.toString();
         int length = word.length()-1;
         int hash=0;
-        for(int x=0, y=length; x<=length; x++, y--){
-            hash+=word.charAt(x)*powers[y];
+        for(int x=0, y=2; x<=length; x++, y--){
+            if(x<3){
+                hash+=word.charAt(x)*powers[y];
+            } else {
+                hash+=word.charAt(x);
+            }
         }
         return hash;
     }
 
-    public static void main(String[] args) {
-
-    }
-
-    class LinkedList<K, V> {
-
-        private Node<K, V> head;
-        private Node<K, V> tail;
-
-        public void add(Node<K, V> node){
-            if(head==null){
-                head = tail = node;
-            } else {
-                tail.next = node;
-                tail = node;
-            }
-        }
-
-        public V remove(){
-            if(head==null){
-                return null;
-            } else {
-                V value = head.value;
-                head = head.next;
-                if(head != null){
-                    head.prev = null;
-                }
-                return value;
-            }
-        }
-        public boolean isEmpty(){
-            return head == null;
-        }
-    }
-
     class Node<K, V>{
-        public K key;
-        public V value;
-        public Node<K, V> next;
-        public Node<K, V> prev;
+        K key;
+        V value;
+        Node<K, V> next;
+        Node<K, V> prev;
 
         public Node(K key, V value) {
             this.key = key;
             this.value = value;
+        }
+    }
+
+    public static void main(String[] args) {
+        HashTable<String, String> mgaWords = new HashTable<>(10);
+        mgaWords.put("Mapa", "Map");
+        mgaWords.put("Sansinukob", "Universe");
+        mgaWords.put("Daigdig", "Earth");
+        mgaWords.put("Daigdig", "World");
+        mgaWords.put("Arkamad", "Pluto");
+        System.out.println(mgaWords.get("Arkamad"));
+
+        HashTable<Double, Double> mgaDouble = new HashTable<>(10);
+        mgaDouble.put(1.5, 1.5*2);
+        mgaDouble.put(1.75, 1.75*2);
+        mgaDouble.put(2.0, 2.0*2);
+        mgaDouble.put(2.0, 3.0*2);
+
+        HashTable<Person, Integer> mgaPeople = new HashTable<>(10);
+        Person[] people = new Person[]{new Person("A"), new Person("B"), new Person("C")};
+        mgaPeople.put(people[0], 25);
+        mgaPeople.put(people[1], 20);
+        mgaPeople.put(people[2], 25);
+        mgaPeople.put(people[2], 26);
+        mgaPeople.put(null, null);
+        mgaPeople.put(null, 16);
+
+        HashTable<String, Integer> map = new HashTable<>(1);
+        String name = "Tristan";
+        map.put(name, 25);
+        map.put("John", 25);
+        map.put("Side", 25);
+        System.out.println(map.get(name));
+        map.remove("Tristan");
+        System.out.println(map.get(name));
+    }
+
+    public static class Person {
+        String name;
+        Person(String name){
+            this.name = name;
         }
     }
 }
