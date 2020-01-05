f8c77c0fc3950843756c5225342b8f29aadd46dd
Christian <christianleroydev@gmail.com>
2020-01-05 12:39:02 +0800

    initial NextWordPredictiom

--- src/main/java/com/theclcode/unfinished/study/nextwordprediction/NextWordPrediction.java
+++ src/main/java/com/theclcode/unfinished/study/nextwordprediction/NextWordPrediction.java
@@ -2,7 +2,7 @@ package com.theclcode.unfinished.study.nextwordprediction;
 
 import java.util.Scanner;
 
-class Main {
+class NextWordPrediction {
 
     /**************** START OF USER SOLUTION ****************/
     
@@ -18,6 +18,153 @@ class Main {
 
         return 0;
     }
+
+
+
+    static class LinkedList<E> {
+		int size;
+		Node<E> head;
+		Node<E> tail;
+
+		public void add(E value){
+			Node<E> node = new Node<>(value);
+			if(head == null){
+				head = tail = node;
+			} else {
+				if(value instanceof Word){
+					Word word = (Word) value;
+					if(size == 5){
+
+					} else {
+						Node<E> existing = head;
+						while(existing != null){
+
+							existing = existing.next;
+						}
+					}
+				} else {
+					tail.next = node;
+					node.prev = tail;
+					tail = node;
+				}
+
+			}
+			size++;
+		}
+
+
+		static class Node<E> {
+			E value;
+			Node<E> prev;
+			Node<E> next;
+
+			Node(E value){
+				this.value = value;
+			}
+		}
+	}
+
+	static class HashTable<K, V>{
+    	int capacity;
+    	Node<K, V>[] table;
+    	static final int BASE = 37;
+    	static final int[] POWERS = {1, BASE, BASE * BASE};
+
+    	public HashTable(){
+    		this(13);
+		}
+
+    	public HashTable(int capacity){
+    		this.capacity = capacity;
+    		this.table = new Node[capacity];
+		}
+
+		public void put(K key, V value){
+    		int index = getAddress(key);
+    		if(table[index] == null){
+    			Node<K, V> node = new Node<>(key, value);
+    			table[index] = node;
+			} else {
+//    			Node<K, V> node = find()
+			}
+		}
+
+		Node<K, V> find(K key){
+    		int index = getAddress(key);
+    		Node<K, V> node = table[index];
+    		while(node != null){
+    			if(key instanceof Word && node.key instanceof String){
+
+				} else if((key == null && node.key == null) || (node.key != null && node.key.equals(key))){
+    				return node;
+				}
+    			node = node.next;
+			}
+    		return null;
+		}
+
+		int getAddress(K key){
+    		return key == null ? 0 : hash(key) % capacity;
+		}
+
+		int hash(K key){
+    		String word = key.toString();
+			int hash = 0;
+    		for(int i=0, y=2; i<word.length(); i++, y--){
+    			if(i < 3){
+    				hash += word.charAt(i) * POWERS[y];
+				} else {
+    				hash += word.charAt(i);
+				}
+			}
+    		return hash;
+		}
+
+
+    	class Node<K, V>{
+    		K key;
+    		V value;
+    		Node<K, V> prev;
+    		Node<K, V> next;
+
+    		Node(K key, V value){
+    			this.key = key;
+    			this.value = value;
+			}
+		}
+	}
+
+	static class Word {
+    	char[] value;
+    	int hash;
+    	static final int BASE = 37;
+    	static final int[] POWERS = {1, BASE, BASE * BASE};
+
+    	Word(char[] value){
+    		this.value = new char[value.length];
+    		for(int i=0; i<value.length && value[i] != '\0'; i++){
+				this.value[i] = value[i];
+			}
+    		setHash();
+		}
+
+		public boolean same(Object obj) {
+    		Word compare = (Word) obj;
+			return false;
+		}
+
+		void setHash(){
+    		int hash = 0;
+    		for(int i=0, y=2; i<this.value.length && this.value[i] != '\0'; i++, y--){
+    			if(i < 3){
+    				hash += this.value[i] * POWERS[y];
+				} else {
+    				hash += this.value[i];
+				}
+			}
+    		this.hash = hash;
+		}
+	}
 
     /***************** END OF USER SOLUTION *****************/
 
