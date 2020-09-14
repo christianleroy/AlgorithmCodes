9af67e9134cccbd82bc19e605fd8465c82e2876d
Christian Leroy Morallos Castillo <cl.castillo@samsung.com>
2020-09-14 15:15:43 +0800

    Wrong SchoolTrouble

--- src/main/java/com/theclcode/unfinished/schooltrouble/SchoolTrouble.java
+++ src/main/java/com/theclcode/unfinished/schooltrouble/SchoolTrouble.java
@@ -9,36 +9,131 @@ public class SchoolTrouble {
 
     /**************** START OF USER SOLUTION ****************/
 
-    static void init(int N) {
+    private static Node[] kids;
 
+    private static void init(int N) {
+        kids = new Node[N+1];
     }
 
-    static boolean setFriends(int kid1, int kid2) {
+    private static boolean setFriends(int kid1, int kid2) {
+        Node k1 = kids[kid1] != null ? kids[kid1] : (kids[kid1] = new Node(kid1));
+        Node k2 = kids[kid2] != null ? kids[kid2] : (kids[kid2] = new Node(kid2));
 
+        if(k1 == k2){
+            return true;
+        }
+
+        Node k1Parent = k1.getParent();
+        Node k2Parent = k2.getParent();
+
+        if(k1Parent != k2Parent && !areEnemies(k1Parent.value, k2Parent.value)){
+            if(k1Parent.getEnemy() != null && k2Parent.getEnemy() != null){
+                k1Parent.getEnemy().absorb(k2Parent.getEnemy());
+                k1Parent.absorb(k2Parent);
+            } else if(k1Parent.getEnemy() != null){
+                k1Parent.absorb(k2Parent);
+            } else {
+                k2Parent.absorb(k1Parent);
+            }
+            return true;
+        }
         return false;
     }
 
-    static boolean setEnemies(int kid1, int kid2) {
+    private static boolean setEnemies(int kid1, int kid2) {
+        Node k1 = kids[kid1] != null ? kids[kid1] : (kids[kid1] = new Node(kid1));
+        Node k2 = kids[kid2] != null ? kids[kid2] : (kids[kid2] = new Node(kid2));
 
+        if(k1 == k2){
+            return false;
+        }
+
+        Node k1Parent = k1.getParent();
+        Node k2Parent = k2.getParent();
+
+        if(k1Parent != k2Parent && !areFriends(k1Parent.value, k2Parent.value)){
+            if(k1Parent.getEnemy() != null && k2Parent.getEnemy() != null){
+                k1Parent.getEnemy().absorb(k2Parent);
+                k2Parent.getEnemy().absorb(k1Parent);
+            } else if(k1Parent.getEnemy() != null){
+                k1Parent.getEnemy().absorb(k2Parent);
+            } else if(k2Parent.getEnemy() != null){
+                k2Parent.getEnemy().absorb(k1Parent);
+            } else {
+                k1Parent.setEnemy(k2Parent);
+                k2Parent.setEnemy(k1Parent);
+            }
+            return true;
+        }
         return false;
     }
 
-    static boolean areFriends(int kid1, int kid2) {
+    private static boolean areFriends(int kid1, int kid2) {
+        Node k1 = kids[kid1] != null ? kids[kid1] : (kids[kid1] = new Node(kid1));
+        Node k2 = kids[kid2] != null ? kids[kid2] : (kids[kid2] = new Node(kid2));
 
-        return false;
+        if(k1 == k2){
+            return true;
+        }
+        Node k1Parent = k1.getParent();
+        Node k2Parent = k2.getParent();
+
+        return k1Parent == k2Parent;
     }
 
-    static boolean areEnemies(int kid1, int kid2) {
+    private static boolean areEnemies(int kid1, int kid2) {
+        Node k1 = kids[kid1] != null ? kids[kid1] : (kids[kid1] = new Node(kid1));
+        Node k2 = kids[kid2] != null ? kids[kid2] : (kids[kid2] = new Node(kid2));
 
-        return false;
+        if(k1 == k2){
+            return false;
+        }
+
+        Node k1Parent = k1.getParent();
+        Node k2Parent = k2.getParent();
+
+        return k1Parent.getEnemy() == k2Parent || k2Parent.getEnemy() == k1Parent;
+    }
+
+    static class Node {
+        int value;
+        Node parent;
+        Node enemy;
+
+        Node(int value){
+            this.value = value;
+        }
+
+        public Node getParent() {
+            if(this.parent == null) {
+                return this;
+            }
+            return this.parent;
+        }
+
+        Node getEnemy(){
+            return this.getParent().enemy;
+        }
+
+        public void setEnemy(Node other){
+            this.enemy = other;
+        }
+
+        public void absorb(Node other){
+            Node thisParent = getParent();
+            Node otherParent = other.getParent();
+            if(thisParent != otherParent){
+                otherParent.parent = thisParent;
+            }
+        }
     }
 
     /***************** END OF USER SOLUTION *****************/
 
-    private final static int SET_FRIENDS = 100;
-    private final static int SET_ENEMIES = 200;
-    private final static int ARE_FRIENDS = 300;
-    private final static int ARE_ENEMIES = 400;
+    private static final int SET_FRIENDS = 100;
+    private static final int SET_ENEMIES = 200;
+    private static final int ARE_FRIENDS = 300;
+    private static final int ARE_ENEMIES = 400;
 
     private static void run(BufferedReader br) throws Exception {
         StringTokenizer st;
