a5ab9ef6084442d6dc264b18dafdd0afc68e124b
Christian <christianleroydev@gmail.com>
2020-11-03 13:56:15 +0800

    Add Sandbox code for BST

---
+++ src/main/java/com/theclcode/smallproblems/pairwithsum/PairWithSum.java
@@ -0,0 +1,31 @@
+package com.theclcode.smallproblems.pairwithsum;
+
+public class PairWithSum {
+
+    public static void main(String[] args) {
+        sorted();
+    }
+
+    private static void unsorted(){
+
+    }
+
+    private static void sorted() {
+        int[] a = {1, 2, 3, 5};
+        int b = 5;
+        for(int i=0, y=a.length-1; i<a.length;){
+            if(i == y){
+                break;
+            }
+            if(a[i] + a[y] > b){
+                y--;
+            } else if(a[i] + a[y] < b){
+                i++;
+            } else {
+                System.out.println("Found!");
+                break;
+            }
+        }
+    }
+
+}
