8a6b3fc115d3327246e34f661ff4e2536ea19820
Christian Leroy M. Castillo <cl.castillo@samsung.com>
2021-02-05 17:23:43 +0800

    Update BinarySearch

--- src/main/java/com/theclcode/searching/binarysearch/BinarySearch.java
+++ src/main/java/com/theclcode/searching/binarysearch/BinarySearch.java
@@ -27,4 +27,32 @@ public class BinarySearch {
             return binarySearch(arr, middle + 1, end, item);
         }
     }
+
+
+    public static void mainSimplified(){
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println(bst(arr, 0, 4, 1));
+        System.out.println(bst(arr, 0, 4, 2));
+        System.out.println(bst(arr, 0, 4, 3));
+        System.out.println(bst(arr, 0, 4, 4));
+        System.out.println(bst(arr, 0, 4, 5));
+        System.out.println(bst(arr, 0, 4, 50));
+    }
+
+    //Simplified
+    public static int bst(int[] arr, int start, int end, int item){
+        if(start < end){
+            int middle = (start + end) / 2;
+            if(item == arr[middle]){
+                return middle;
+            } else {
+                if(item < arr[middle]){
+                    return bst(arr, start, middle - 1, item);
+                } else {
+                    return bst(arr, middle + 1, end, item);
+                }
+            }
+        }
+        return arr[start] == item ? start : -1;
+    }
 }
