af64165c736d122a918d372af59f53b68eba3b2b
Christian Leroy Castillo <christianleroydev@gmail.com>
2021-03-27 11:37:02 +0800

    Update ClimbingStairs

--- src/main/java/com/theclcode/smallproblems/climbingstairs/ClimbingStairs.java
+++ src/main/java/com/theclcode/smallproblems/climbingstairs/ClimbingStairs.java
@@ -1,29 +1,42 @@
 package com.theclcode.smallproblems.climbingstairs;
 
-//fibonacci (modified)
+//fibonacci
 public class ClimbingStairs {
     private Integer[] table;
 
     public static void main(String[] args) {
         System.out.println(new ClimbingStairs().climbStairs(5));
         System.out.println(new ClimbingStairs().climbStairs(2));
-        System.out.println(new ClimbingStairs().climbStairs(3));
+        System.out.println(new ClimbingStairs().climbStairs(0));
+        System.out.println(new ClimbingStairs().climbStairs(1));
     }
 
-    public int climbStairs(int n){
-        this.table = new Integer[n+1];
-        return fib(n);
-    }
-
-    public int fib(int n){
-        if(n < 0){
+    public int climbStairs(int n) {
+        if(n == 0) {
             return 0;
-        } else if(n == 0){
+        }
+        this.table = new Integer[n + 1];
+        return fiboRecursive(n);
+    }
+
+    public int fiboDp(int n) {
+        if (n < 0) {
+            return 0;
+        } else if (n == 0) {
             return 1;
         }
-        if(table[n] == null) {
-            table[n] = fib(n - 1) + fib(n - 2);
+        if (table[n] == null) {
+            table[n] = fiboDp(n - 1) + fiboDp(n - 2);
         }
         return table[n];
+    }
+
+    public static int fiboRecursive(int n) {
+        if (n < 0) {
+            return 0;
+        } else if (n == 0) {
+            return 1;
+        }
+        return fiboRecursive(n - 1) + fiboRecursive(n - 2);
     }
 }
