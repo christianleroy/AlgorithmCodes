d46caa620211d4ffc1970cb8f10cde38fa324666
Christian Leroy Castillo <42598080+christianleroy@users.noreply.github.com>
2025-01-16 12:38:46 +0800

    add codes

---
+++ src/main/java/com/theclcode/searching/graph/medium/CourseSchedule.java
@@ -0,0 +1,83 @@
+package com.theclcode.searching.graph.medium;
+
+import java.util.*;
+
+public class CourseSchedule {
+
+    public static void main(String[] args) {
+
+        int[][] courses = {
+                {1, 7},
+                {7, 0},
+                {1, 0},
+                {0, 5}
+        };
+
+        System.out.println(canFinish(8, courses));
+
+//        canFinish(8, courses);
+    }
+
+    public static boolean canFinish(int numCourses, int[][] prerequisites) {
+
+        Map<Integer, Node> map = new HashMap<>();
+
+        for(int i = 0; i < numCourses; i ++){
+            map.put(i, new Node(i));
+        }
+
+        for(int i = 0; i < prerequisites.length; i++) {
+            Node node = map.get(prerequisites[i][0]);
+            Node prereq = map.get(prerequisites[i][1]);
+            node.addPrereq(prereq);
+        }
+
+        Set<Integer> explored = new HashSet<>();
+        for(Map.Entry<Integer, Node> entry: map.entrySet()) {
+            if(!explored.contains(entry.getKey()) &&
+                    hasCycle(entry.getValue(), explored, new HashSet<>())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean hasCycle(Node node, Set<Integer> visited, Set<Integer> recursionPath) {
+
+        if(recursionPath.contains(node.value)){
+            return true; // already in the current path, can confirm cycle exists
+        }
+
+        if(visited.contains(node.value)) {
+            return false; // explored, no cycle
+        }
+
+        recursionPath.add(node.value);
+        visited.add(node.value);
+
+        for(Node prereq : node.prereqs) {
+            if(hasCycle(prereq, visited, recursionPath)) {
+                return true;
+            }
+        }
+
+        recursionPath.remove(node.value);
+        return false;
+
+    }
+
+    static class Node {
+        int value;
+        private List<Node> prereqs;
+
+        Node(int value) {
+            this.value = value;
+            this.prereqs = new ArrayList<>();
+        }
+
+        void addPrereq(Node node) {
+            prereqs.add(node);
+        }
+
+    }
+}
--- src/main/java/com/theclcode/test/Sandbox.java
+++ src/main/java/com/theclcode/test/Sandbox.java
@@ -1,6 +1,134 @@
 package com.theclcode.test;
 
+import java.util.*;
+
 public class Sandbox {
 
+    public static void main(String[] args) {
 
+//        {1,5},{6,9}
+//        int[][] intervals = {{1,3},{6,9}};
+//        int[] newInterval = {2,5};
+
+//         {1,2},{3,10},{12,16}
+//        int[][] intervals = {{1,2},{3,5},{6,7},{8,10},{12,16}};
+//        int[] newInterval = {4,8};
+
+//        newInterval within or beyond last intervals element
+//        {3,6},{7,11},{12,13}
+//        int[][] intervals = {{3,6},{7,11}};
+//        int[] newInterval = {12,13};
+
+//        {3,6},{7,13}
+//        int[][] intervals = {{3,6},{7,11}};
+//        int[] newInterval = {10,13};
+
+//        newInterval within or before the first intervals element
+//        {1,6},{7,11}
+//        int[][] intervals = {{3,6},{7,11}};
+//        int[] newInterval = {1,4};
+
+//        {1,2},{3,6},{7,11}
+//        int[][] intervals = {{3,6},{7,11}};
+//        int[] newInterval = {1,2};
+
+        // Beyond range
+//        int[][] intervals = {{3,6},{7,11}};
+//        int[] newInterval = {1,15};
+
+//        {2,15}
+//        int[][] intervals = {{3,6},{7,11}};
+//        int[] newInterval = {2,15};
+
+//        {5,15}
+        int[][] intervals = {{3,6},{7,11}};
+        int[] newInterval = {5,15};
+
+//        {1,11}
+//        int[][] intervals = {{3,6},{7,11}};
+//        int[] newInterval = {1,10};
+
+
+        int[][] res = insert(intervals, newInterval);
+
+        for(int i = 0; i < res.length; i++) {
+            System.out.println("{" + res[i][0] + "," + res[i][1] + "}");
+        }
+    }
+
+    public static int[][] insert(int[][] intervals, int[] newInterval) {
+
+        int newStart = newInterval[0];
+        int newEnd = newInterval[1];
+        int startIdx = -1;
+        int endIdx = -1;
+
+        List<Integer[]> ints = new ArrayList<>();
+
+        if(newStart < intervals[0][0] && newEnd > intervals[intervals.length-1][1]) {
+            return new int[][]{{newStart, newEnd}};
+        }
+
+        if(newEnd < intervals[0][0]) {
+            ints.add(new Integer[]{newStart, newEnd});
+            startIdx = 0;
+            endIdx = 0;
+        } else if(newStart > intervals[intervals.length-1][1]) {
+            startIdx=0;
+            endIdx = 0;
+        }else {
+
+            if(newStart < intervals[0][0]) {
+                startIdx = 0;
+                intervals[0][0] = newStart;
+            }
+
+            for(int i = 0; endIdx == -1 && i < intervals.length; i++) {
+                if(startIdx == -1) {
+                    if(newStart >= intervals[i][0] && newStart <= intervals[i][1]) {
+                        startIdx = i;
+                    } else {
+                        Integer[] interval = new Integer[]{intervals[i][0], intervals[i][1]};
+                        ints.add(interval);
+                    }
+                } else {
+                    if(newEnd < intervals[i][0]) {
+                        Integer[] interval = new Integer[]{intervals[startIdx][0], Math.max(newEnd, intervals[i - 1][1])};
+                        ints.add(interval);
+                        endIdx = i;
+                    } else if(newEnd >= intervals[i][0] && newEnd <= intervals[i][1]) {
+                        Integer[] interval = new Integer[]{intervals[startIdx][0], Math.max(newEnd, intervals[i][1])};
+                        ints.add(interval);
+                        endIdx = i+1;
+                    }
+                }
+            }
+            if(endIdx == -1) {
+                endIdx = startIdx + 1;
+                intervals[intervals.length-1][1] = Math.max(newEnd, intervals[intervals.length-1][1]);
+            }
+        }
+
+        for(int j = endIdx; j < intervals.length; j++) {
+//            if(startIdx == -1 && j == intervals.length-1) {
+//                break;
+//            }
+            ints.add(new Integer[]{intervals[j][0], intervals[j][1]});
+        }
+
+//        if(startIdx == -1) {
+//            ints.add(new Integer[]{Math.min(newStart, intervals[intervals.length-1][0]), newEnd});
+//        } else if(newStart > intervals[intervals.length-1][1]) {
+//            ints.add(new Integer[]{newStart, newEnd});
+//        }
+
+        int[][] result = new int[ints.size()][2];
+
+        for(int i = 0; i < result.length; i++) {
+            result[i][0] = ints.get(i)[0];
+            result[i][1] = ints.get(i)[1];
+        }
+
+        return result;
+    }
 }
